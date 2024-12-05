47bc7add57e04a2a998d5992074df7c4b504f1aa
michee-04 <nangmamichee0@gmail.com>
2024-12-05 12:28:42 +0000

    feat: implementation du model et du type des workflow

---
+++ src/workflow/domain/model/workflow.model.ts
@@ -0,0 +1,64 @@
+import { BaseModel, createBaseSchema } from '@nodesandbox/repo-framework';
+import { IWorkflowModel } from '../types';
+
+export const WORKFLOW_MODEL_NAME = 'Workflow';
+
+const workflowSchema = createBaseSchema<IWorkflowModel>(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    service: {
+      type: String,
+      required: true,
+    },
+    schemas: {
+      xml: { type: String },
+      json: { type: String },
+    },
+    access: {
+      groups: {
+        type: String,
+        enum: ['admins', 'managers'],
+      },
+      roles: {
+        type: String,
+        enum: ['admin', 'editor'],
+      },
+    },
+    active: {
+      type: Boolean,
+    },
+    version: {
+      type: Number,
+    },
+    tag: {
+      type: String,
+      enum: ['approval', 'finance'],
+    },
+    status: {
+      type: String,
+      enum: ['active', 'inactive'],
+      default: 'inactive',
+    },
+  },
+  {
+    modelName: WORKFLOW_MODEL_NAME,
+  },
+);
+
+const workflowModel = new BaseModel<IWorkflowModel>(
+  WORKFLOW_MODEL_NAME,
+  workflowSchema,
+).getModel();
+
+export { workflowModel, workflowSchema };
